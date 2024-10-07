f233d6c8bf9afda98da60a99a352607565632df5
Aram Molins <aram.molins@gmail.com>
2024-10-07 16:25:27 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,7 +36,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Web estàtica generada {currentYear} a partir de <u><a href="https://fisicaubwiki.notion.site">fisicaubwiki.notion.site</a></u> mitjançant.</div>
+      <div className={styles.copyright}>Web estàtica generada {currentYear} a partir de <u style="color: rgb(51, 126, 169)"><a href="https://fisicaubwiki.notion.site">fisicaubwiki.notion.site</a></u> mitjançant.</div>
 
       <div className={styles.settings}>
         {hasMounted && (
