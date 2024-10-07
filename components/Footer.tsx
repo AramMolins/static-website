059b9ca15216221141ba8b5e5f3230cdc1c2cea0
Aram Molins <aram.molins@gmail.com>
2024-10-07 15:57:45 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,7 @@ import styles from './styles.module.css'
 export const FooterImpl: React.FC = () => {
   const [hasMounted, setHasMounted] = React.useState(false)
   const { isDarkMode, toggleDarkMode } = useDarkMode()
-  // const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear()
 
   const onToggleDarkMode = React.useCallback(
     (e) => {
@@ -36,8 +36,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Web estatica generada a partir de fisicaubwiki.notion.cite
-      </div>
+      <div className={styles.copyright}>Copyright {currentYear} {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
