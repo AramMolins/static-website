d8378b6a59ceb0edded6615a7c4846c2ddd2b603
Aram Molins <aram.molins@gmail.com>
2024-10-07 10:31:22 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Sobre aquesta web',
+      pageId: '118cda9984bf80e8beeadac0f928ba3c'
+    },
+    {
+      title: 'Contacte',
+      pageId: '118cda9984bf80eeb5b1d94c018c5342'
+    }
+  ]
 })
