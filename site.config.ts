33fea74360f8e2ef21f4d442fe29fe02438dcb6c
Aram Molins <aram.molins@gmail.com>
2025-02-22 18:39:44 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: '',
-  github: 'AramMolins/static-webiste',
+  github: 'AramMolins/static-website',
   linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
